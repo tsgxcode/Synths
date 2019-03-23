d8997afba043f19055fb5b41f612d16739548030
tsgcats@gmail.com <tsgcats@gmail.com>
2019-03-23 07:37:01 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynthWave
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            
+            //Prompt user for number wave forms
+            Console.WriteLine("How many wave forms would you like? or type \"quit\" to exity");
+            string entry = Console.ReadLine();
+
+            //Display number of Waveforms
+            Console.WriteLine("You've chosen " + entry + " Waveforms" + " from the Fairlight CMI");
+
+            Console.ReadLine();
+
+        }
+    } 
+}
